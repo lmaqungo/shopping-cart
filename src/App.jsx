65440b54e974f030ed2b1dcde21eeed2365cc461
luya <91703950+lmaqungo@users.noreply.github.com>
2026-06-29 14:55:40 +0200

    complete the tag object array

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,22 @@ function App() {
       id: 1, 
       title: 'rectangular', 
       isActive: false
-    }
+    }, 
+    {
+      id: 2, 
+      title: 'plate', 
+      isActive: false
+    }, 
+    {
+      id: 3, 
+      title: 'rounded', 
+      isActive: false
+    }, 
+    {
+      id: 4, 
+      title: 'triangular', 
+      isActive: false
+    }, 
   ]
 
   const location = useLocation(); 
